789dd21cee5a1c508c30bbeaf8d374ac3b9ee0ca
Mohammad-Ismail-Dev <mohamad.ismail.dev@gmail.com>
2023-11-22 17:42:23 +0200

    box widget and 3x3 grid

--- lib/box.dart
+++ lib/box.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
 
+class BoxWidget extends StatefulWidget {
+  final String text;
+  const BoxWidget(this.text);
+
+  @override
+  State<BoxWidget> createState() => _BoxWidgetState('');
+}
+
+class _BoxWidgetState extends State<BoxWidget> {
+  String text;
+  bool pressed = false;
+  _BoxWidgetState(this.text);
+
+  String value = '';
+  List<String> values = ['X', 'O'];
+  int count = 0;
+  void toggleDraw() {
+    setState(() {
+      if (pressed == false) {
+        text = values[0];
+        pressed = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder(
+      child: ElevatedButton(
+          onPressed: toggleDraw,
+          child: SizedBox(
+              height: 300, width: 300, child: Center(child: Text(text)))),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:project/box.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: SizedBox(
+          height: 300,
+          width: 300,
+          child: GridView.count(
+              // Create a grid with 2 columns. If you change the scrollDirection to
+              // horizontal, this produces 2 rows.
+              crossAxisCount: 3,
+              // Generate 100 widgets that display their index in the List.
+              children: [
+                BoxWidget("O"),
+                BoxWidget("x"),
+                BoxWidget("x"),
+                BoxWidget("x"),
+                BoxWidget("x"),
+                BoxWidget("x"),
+                BoxWidget("x"),
+                BoxWidget("x"),
+                BoxWidget("x"),
+              ]),
+        ));
   }
 }
