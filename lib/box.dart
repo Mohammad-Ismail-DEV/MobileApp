d19dc88a4c5007df84904b58afbdceb3cac357cd
halaghz <halaghzayel@gmail.com>
2023-11-27 18:38:01 +0200

    added methods to show win or draw

--- lib/box.dart
+++ lib/box.dart
@@ -24,6 +24,96 @@ class _BoxWidgetState extends State<BoxWidget> {
         globals.xTurn = !globals.xTurn;
       }
     });
+
+    _checkWinner();
+  }
+
+  void _showWinDialog(String winner) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("\" $winner \" is Winner!!!"),
+            actions: [
+              ElevatedButton(
+                child: const Text("Play Again"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  void _showDrawDialog() {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Draw"),
+            actions: [
+              ElevatedButton(
+                child: const Text("Play Again"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  void _checkWinner() {
+    // Checking rows
+    if (globals.displayElement[0] == globals.displayElement[1] &&
+        globals.displayElement[0] == globals.displayElement[2] &&
+        globals.displayElement[0] != '') {
+      _showWinDialog(globals.displayElement[0]);
+    }
+    if (globals.displayElement[3] == globals.displayElement[4] &&
+        globals.displayElement[3] == globals.displayElement[5] &&
+        globals.displayElement[3] != '') {
+      _showWinDialog(globals.displayElement[3]);
+    }
+    if (globals.displayElement[6] == globals.displayElement[7] &&
+        globals.displayElement[6] == globals.displayElement[8] &&
+        globals.displayElement[6] != '') {
+      _showWinDialog(globals.displayElement[6]);
+    }
+
+    // Checking Column
+    if (globals.displayElement[0] == globals.displayElement[3] &&
+        globals.displayElement[0] == globals.displayElement[6] &&
+        globals.displayElement[0] != '') {
+      _showWinDialog(globals.displayElement[0]);
+    }
+    if (globals.displayElement[1] == globals.displayElement[4] &&
+        globals.displayElement[1] == globals.displayElement[7] &&
+        globals.displayElement[1] != '') {
+      _showWinDialog(globals.displayElement[1]);
+    }
+    if (globals.displayElement[2] == globals.displayElement[5] &&
+        globals.displayElement[2] == globals.displayElement[8] &&
+        globals.displayElement[2] != '') {
+      _showWinDialog(globals.displayElement[2]);
+    }
+
+    // Checking Diagonal
+    if (globals.displayElement[0] == globals.displayElement[4] &&
+        globals.displayElement[0] == globals.displayElement[8] &&
+        globals.displayElement[0] != '') {
+      _showWinDialog(globals.displayElement[0]);
+    }
+    if (globals.displayElement[2] == globals.displayElement[4] &&
+        globals.displayElement[2] == globals.displayElement[6] &&
+        globals.displayElement[2] != '') {
+      _showWinDialog(globals.displayElement[2]);
+    } else if (globals.filledBoxes == 9) {
+      _showDrawDialog();
+    }
   }
 
   @override
